4c06d2dfbef8d893fbf590f970438341fcc2f2b3
andreabarbiero <andrea.barbiero1@gmail.com>
2023-07-08 17:54:28 +0200

    sostituito array con csv

--- Manage_User/README.md
+++ Manage_User/README.md
@@ -2,3 +2,11 @@
 
 Questo script automatizza la creazione di utenti su Gitlab.
 
+Ho modificato lo script in modo tale che prenda la lista utenti ed i dati come email e password da un file CSV,
+i campi al momento sono 3 username, email e password ma dato che il profilo utente Gitlab ha molte informazioni, 
+e' possibile aggiungere i campi che si vuole, basta aggiungere poi le variabili nella prima for dove viene letto il file CSV.
+
+```shell
+  pip3 install requests
+  pip3 install csv
+```
--- Manage_User/add_user_manteiner.py
+++ Manage_User/add_user_manteiner.py
@@ -3,42 +3,49 @@ import requests
 gitlab_url = "https://SOSTITUIRE_GITLAB_URL/api/v4"  # Aggiorna con l'URL del tuo GitLab CE
 access_token = "TOKEN_CON_PERMESSI_API"  # Aggiorna con il tuo token di accesso
 
-user_list = ["topolino.disney","paperino.disney","pluto.disney"]  # Aggiorna con la tua lista di nomi utente
+csv_file = "users.csv"
 
-for username in user_list:
-    # Creazione dell'utente
-    user_endpoint = f"{gitlab_url}/users"
-    headers = {
-        "Private-Token": access_token,
-        "Content-Type": "application/json"
-    }
-    user_data = {
-        "username": username,
-        "password": "PASSWORD_INIZIALE_UTENTE",  # Aggiorna con la password desiderata
-        "email": f"{username}@TUO_DOMINIO.it",  # Aggiorna con l'email desiderata
-        "name": username,  # Aggiungi il campo "name" con il valore dello username
-        "skip_confirmation": True,  # Imposta il flag "skip_confirmation" su True
-    }
+with open(csv_file, newline='') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        username = row['username']
+        email = row['email']
+        password = row['password']
 
-    user_response = requests.post(user_endpoint, headers=headers, json=user_data)
 
-    if user_response.status_code == 201:
-        print(f"Utente {username} creato con successo.")
-    else:
-        print(f"Errore durante la creazione dell'utente {username}: {user_response.text}")
-        continue
+      # Creazione dell'utente
+      user_endpoint = f"{gitlab_url}/users"
+      headers = {
+          "Private-Token": access_token,
+          "Content-Type": "application/json"
+      }
+      user_data = {
+          "username": username,
+          "password": password,
+          "email": email,
+          "name": username,  
+          "skip_confirmation": True
+      }
 
-    # Impostazione del flag "password_automatically_set" per il primo accesso
-    user_id = user_response.json()["id"]
-    user_update_endpoint = f"{gitlab_url}/users/{user_id}"
-    user_update_data = {
-        "password_automatically_set": True
-    }
+      user_response = requests.post(user_endpoint, headers=headers, json=user_data)
 
-    user_update_response = requests.put(user_update_endpoint, headers=headers, json=user_update_data)
+      if user_response.status_code == 201:
+          print(f"Utente {username} creato con successo.")
+      else:
+          print(f"Errore durante la creazione dell'utente {username}: {user_response.text}")
+          continue
 
-    if user_update_response.status_code == 200:
-        print(f"Impostato il flag 'password_automatically_set' per l'utente {username}.")
-    else:
-        print(f"Errore durante l'impostazione del flag 'password_automatically_set' per l'utente {username}: {user_update_response.text}")
+      # Impostazione del flag "password_automatically_set" per il primo accesso
+      user_id = user_response.json()["id"]
+      user_update_endpoint = f"{gitlab_url}/users/{user_id}"
+      user_update_data = {
+          "password_automatically_set": True
+      }
+
+      user_update_response = requests.put(user_update_endpoint, headers=headers, json=user_update_data)
+
+      if user_update_response.status_code == 200:
+          print(f"Impostato il flag 'password_automatically_set' per l'utente {username}.")
+      else:
+          print(f"Errore durante l'impostazione del flag 'password_automatically_set' per l'utente {username}: {user_update_response.text}")
     
